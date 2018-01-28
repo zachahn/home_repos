66e1b4466a442b5efb457882bb223de59979f166
Zach Ahn <engineering@zachahn.com>
2018-01-27 21:25:22 -0500

    Annotate models :)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,14 @@
+# == Schema Info
+#
+# Table name: projects
+#
+#   id            integer     not null, primary key
+#   path          string
+#   created_at    datetime    not null
+#   updated_at    datetime    not null
+#   export        boolean
+#
+
 class Project < ApplicationRecord
   def name
     @name ||= File.basename(path, ".git")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Info
+#
+# Table name: users
+#
+#   id                 integer     not null, primary key
+#   email              string
+#   password_digest    string
+#   expires_at         datetime
+#   created_at         datetime    not null
+#   updated_at         datetime    not null
+#   admin              boolean
+#
+
 class User < ApplicationRecord
   has_secure_password
 end
