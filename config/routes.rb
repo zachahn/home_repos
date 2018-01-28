4a6a1b1a0a52432f477db6401112dadeac2364e0
Zach Ahn <engineering@zachahn.com>
2018-01-28 18:44:41 -0500

    Add "todo" routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :projects, param: :name
+  root to: "projects#index"
+  resources :projects, param: :name, path: "/" do
+    get "objects/:reference(/*path)", to: "objects#show"
+    resources :commits, param: :hash, only: [:show]
+    resources :branches, param: :name, only: [:index]
+  end
   resources :users
   resource :session, only: %i[create destroy]
 end
