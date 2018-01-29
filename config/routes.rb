6ec636c65ebfb341e8506eb7910a99d47a74e314
Zach Ahn <engineering@zachahn.com>
2018-01-28 21:54:22 -0500

    Fix a test
    Order dependent routes? This is weird, but I didn't investigate

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: "projects#index"
+  resource :session, only: %i[create destroy]
   resources :projects, param: :name, path: "/" do
     get "objects/:reference(/*path)", to: "objects#show"
     resources :commits, param: :hash, only: [:show]
     resources :branches, param: :name, only: [:index]
   end
-  resource :session, only: %i[create destroy]
 end
