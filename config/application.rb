57c9d7f52cbbaac005f35908eeb69880d61c8e59
Zach Ahn <engineering@zachahn.com>
2018-04-15 00:49:21 -0400

    Upgrade Rails configuration
    `rake app:update`

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module HomeRepos
     config.active_job.queue_adapter = :delayed_job
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
